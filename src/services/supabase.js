4519cfd33e49c2a9a668d9243554f0b637cb143f
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2025-10-26 15:36:44 +0700

    Cleaning up

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-export const supabaseUrl = 'https://tydjcwzfsdethwseawox.supabase.co';
-const supabaseKey =
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR5ZGpjd3pmc2RldGh3c2Vhd294Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NTU5NzgsImV4cCI6MjA3MDEzMTk3OH0.e1bwZ3S3KVjRW5sxTMWB3lKc9IsLygUlv0VMLial5jA';
-const supabase = createClient(supabaseUrl, supabaseKey);
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
